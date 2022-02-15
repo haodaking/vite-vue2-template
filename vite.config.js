6107b028b524d0227dd5a702293c872749df4f1e
wangpengfei <wangpengfei@ezijing.com>
2022-02-15 17:11:59 +0800

    chore: 升级相关依赖模块；@rollup/plugin-eslint -> vite-plugin-checker;

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
-import eslint from '@rollup/plugin-eslint'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { open: true },
-  plugins: [eslint({ include: '**/*.+(vue|js|jsx|ts|tsx)' }), createVuePlugin()]
+  plugins: [
+    checker({
+      eslint: { lintCommand: 'eslint "./src/**/*.{vue,js,jsx,ts,tsx}"' }
+    }),
+    createVuePlugin()
+  ]
 })
